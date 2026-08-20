8a6b93a763ec3e86930f8979f50d626130e3557c
gump <contact@gump.dev>
2026-08-19 23:34:43 -0300

    update build debug

--- scripts/build-release/win.cmd
+++ scripts/build-release/win.cmd
@@ -1,7 +1,7 @@
 echo Building Sucata for Windows...
 
 echo Building sucata-player.exe...
-odin build src/ -out:sucata-player.exe -o:speed
+odin build src/ -out:sucata-player.exe -o:speed -extra-linker-flags:"/VERBOSE:LIB" 2>&1
 
 echo Done!
 
