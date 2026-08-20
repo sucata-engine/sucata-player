4a86ac14922936e8cb8036faab2af93f4de6eef3
gump <contact@gump.dev>
2026-08-19 23:39:13 -0300

    fix build windows

--- scripts/build-release/win.cmd
+++ scripts/build-release/win.cmd
@@ -1,7 +1,7 @@
 echo Building Sucata for Windows...
 
 echo Building sucata-player.exe...
-odin build src/ -out:sucata-player.exe -o:speed -extra-linker-flags:"/VERBOSE:LIB" 2>&1
+odin build src/ -out:sucata-player.exe -o:speed -extra-linker-flags:"/NODEFAULTLIB:ucrt.lib"
 
 echo Done!
 
